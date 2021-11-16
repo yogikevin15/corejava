0169b8e9df9e411a7acb89f07e5c318ceadc5106
mallikarjun reddy <yogiswarkevin5@gmail.com>
2021-11-16 12:12:47 +0530

    Addition

---
+++ src/com/corejava/Addition.java
@@ -0,0 +1,20 @@
+package com.corejava;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter A value:");
+		int a=s.nextInt();
+		System.out.println("enter B value:");
+		int b=s.nextInt();
+		int c=a+b;
+		
+		System.out.println("addition of a+b "+);
+
+	}
+
+}
---
+++ src/com/corejava/Helloworld.java
@@ -0,0 +1,20 @@
+package com.corejava;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		/* int value1=10;
+		float value2=20f/13f;
+		double value3=20d/13d ;
+		
+		System.out.println("value1   " + value1);
+		System.out.println("value2   " +value2);
+		System.out.println("value3   " +value3);
+		*/
+		int marker = 1000;
+		double Percentage = marker * 0.20f ;
+		System.out.println("percentage "+ Percentage);
+
+	}
+
+}
